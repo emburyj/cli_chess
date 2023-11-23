10d44a9e1ec8783d2d20cd17bc8410074558d84f
emburyj <joshembury@gmail.com>
2023-11-22 20:59:07 -0700

    initial commit

---
+++ ChessVar.py
@@ -0,0 +1,26 @@
+'''
+Author: Josh Embury
+GitHub username: emburyj
+Date: 11/22/23
+Description:
+'''
+class ChessVar(object):
+    def __init__(self):
+        pass
+
+    def get_game_state(self):
+        pass
+
+    def make_move(self, from_square, to_square):
+        pass
+
+class ChessBoard(object):
+    def __init__(self):
+        pass
+
+class ChessPiece(object):
+
+    def __init__(self, color):
+        self._color = color
+        self._name = name
+        self._position = position
