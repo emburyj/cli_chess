73a226582939397b91f8e83253340d99dca74995
emburyj <joshembury@gmail.com>
2023-11-29 20:25:06 -0700

    Added documentation and pseudocode to ChessVar class

--- ChessVar.py
+++ ChessVar.py
@@ -2,16 +2,27 @@
 Author: Josh Embury
 GitHub username: emburyj
 Date: 11/22/23
-Description:
+Description: Defines ChessVar, ChessBoard, and ChessPiece classes and subclasses
 '''
 class ChessVar(object):
     def __init__(self):
-        pass
+        self._game_state = "UNFINISHED"
+        self._gameboard = ChessBoard()
 
     def get_game_state(self):
-        pass
+        '''Getter method for _game_state field'''
+        return self._game_state
 
     def make_move(self, from_square, to_square):
+        '''
+        Method to make a move on the
+        :param from_square: String representing from square
+        :param to_square: String representing to square
+        :return: Boolean
+        '''
+        # checks if game is over
+        # calls make_move method for _gameboard
+        # checks for winner and updates _game_state a/r
         pass
 
 class ChessBoard(object):
@@ -20,7 +31,7 @@ class ChessBoard(object):
         self._white_pieces = 16
         self._black_pieces = 16
         self._board = {} # dict - representing squares on board {str: Chesspiece obj}
-
+        self._turn = "White"
         # initialize board dict with pieces
 
     def get_valid_moves(self, from_square):
@@ -36,12 +47,13 @@ class ChessBoard(object):
         Method to update board for given move
         :param from_square: String representing from square
         :param to_square: String representing to square
-        :return: Void
+        :return: Boolean - False if invalid move is given
         '''
         # checks if move is valid
         # if to_square in self.get_valid_moves(from_square):
         #   do something
         # checks if move is a capture
+        # update turn
         pass
 
     def make_capture(self, from_square, to_square):
@@ -61,6 +73,25 @@ class ChessBoard(object):
         # board_string = "   A   B   C   D   E   F   G   H   \n"
         # for i in range(8, 0, -1):
         #     current_row = f"{i|}"
+
+    def update_turn(self):
+        '''Method to change turn from black/white or white/black'''
+        if self._turn == "Black":
+            self._turn = "White"
+        else:
+            self._turn = "Black"
+
+    def get_turn(self):
+        '''Getter method for _turn field'''
+        return self._turn
+
+    def get_white(self):
+        '''Getter method for _white_pieces field'''
+        return self._white_pieces
+
+    def get_black(self):
+        '''Getter method for _black_pieces field'''
+        return self._black_pieces
 
 
 class ChessPiece(object):
